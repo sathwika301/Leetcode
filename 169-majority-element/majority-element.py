0fbd3a55416eafa918a2dadbbc2f4b07de19c49f
D Sathwika <ugs23301_aiml.dodla@cbit.org.in>
2026-07-05 23:35:22 +0530

    Time: 3 ms (85.32%) | Memory: 21.1 MB (86.31%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,17 @@
            
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        hashmap={}
+        count=0
+        ele=0
         for n in nums:
-            hashmap[n]=hashmap.get(n,0)+1
-      
-        for key, value in hashmap.items():
-            if value>len(nums)//2:
-                return key
+            if count==0:
+                ele=n
+                count=1
+            elif ele==n:
+                count+=1
+            else:
+                count-=1
+        return ele
 sol=Solution()
 res=sol.majorityElement(nums = [3,3,4])
 print(res)   
