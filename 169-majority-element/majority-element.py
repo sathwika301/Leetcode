8753d9e474efc7ecc607f38fa0876b5e5011d823
D Sathwika <ugs23301_aiml.dodla@cbit.org.in>
2026-07-05 23:08:23 +0530

    Time: 4 ms (70.70%) | Memory: 21.3 MB (19.42%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,10 +4,7 @@ class Solution:
         hashmap={}
         for n in nums:
             hashmap[n]=hashmap.get(n,0)+1
-        print(hashmap)
-        print(hashmap.values())
-        m=max(hashmap.values())
-        print(m)
+      
         for key, value in hashmap.items():
             if value>len(nums)//2:
                 return key
