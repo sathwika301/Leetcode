7cae16c4293ae1403f8469f861961ecae10e74da
D Sathwika <ugs23301_aiml.dodla@cbit.org.in>
2026-07-05 19:14:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (53.91%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,27 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def powerf(x,n):
+            if n==1:
+                return x*n
+            else:
+                if n%2==0:
+                    return powerf(x*x, n//2)
+                if n%2!=0:
+                    return x*(powerf(x*x, (n-1)//2))
+
+        if n==0:
+            return 1
+        elif n==1:
+            return x
+        else:
+            if n<0:
+                temp=-1*n
+                return 1/(powerf(x,temp))
+            else:
+                return powerf(x,n)
+                
+sol=Solution()
+r=sol.myPow(x = 2.00000, n = -2)
+print(r)
+
+        
