561b5223a2967cee12145c52b68ab58bdf6130a3
D Sathwika <ugs23301_aiml.dodla@cbit.org.in>
2026-07-15 23:16:47 +0530

    Time: 1 ms (65.11%) | Memory: 20.5 MB (40.09%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n=len(nums)
+        arr_sum = sum(nums)
+        actual_sum=0
         for i in range(n+1):
-            if i not in nums:
-                return i
+            actual_sum += i
+        res= actual_sum - arr_sum
+        return res
+
         
