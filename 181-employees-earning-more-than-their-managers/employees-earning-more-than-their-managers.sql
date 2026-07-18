7189c70615caa5f273b6724dc7ef978bf92bc174
D Sathwika <ugs23301_aiml.dodla@cbit.org.in>
2026-07-18 22:54:05 +0530

    Time: 352 ms (97.36%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 SELECT e.name  as Employee 
 FROM  Employee e JOIN Employee m
-WHERE e.managerId=m.Id and e.salary> m.salary
+ON e.managerId=m.Id 
+WHERE e.salary> m.salary
 
 
