669b010f033b6e84da236d8daca555ef08074350
D Sathwika <ugs23301_aiml.dodla@cbit.org.in>
2026-07-30 22:34:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (16.35%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -1,7 +1,11 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        sum=0
-        for i in range(len(word)):
-            sum+=i//8+1
-        return sum
-        
+        n=len(word)
+        if n<=8:
+            return n
+        elif n<=16:
+            return 8+(n-8)*2
+        elif n<=24:
+            return 8+16+(n-16)*3
+        else:
+            return 8+16+24+(n-24)*4
