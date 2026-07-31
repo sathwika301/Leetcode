e7b0408b583a68eac8996211c8c023ca45cd13b1
D Sathwika <ugs23301_aiml.dodla@cbit.org.in>
2026-07-31 20:56:45 +0530

    Time: 180 ms (34.45%) | Memory: 19.9 MB (93.65%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -3,21 +3,15 @@ class Solution:
         freq={}
         for c in word:
             freq[c]=freq.get(c,0)+1
+
         res=list(freq.values())
         res.sort(reverse=True)
+
         m=len(res)
-        sum=0
-        for j in range(0,m):
-            print("j=",j)
-            if j<8:
-                sum+=1*res[j]
-            elif j>=8 and j<16:
-                sum+=2*res[j]
-            elif j>=16 and j<24:
-                sum+=3*res[j]
-            else:
-                sum+=4*res[j]
-        return sum
+        ans=0
+        for i in range(m):
+            ans += (i // 8 + 1) * res[i]
+        return ans
 sol=Solution()
 res=sol.minimumPushes(word="xyzxyzxyzxyz")
 print(res)
